7d11f28aafb6f78f90644dabee27eab77552e8ce
Artem <artkrasov@gmail.com>
2024-06-30 20:38:56 +0300

    Сделал получение данных с сервера

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,47 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
+import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
+
 // import Drawer from './components/Drawer.vue'
-Header
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get('https://604781a0efa572c1.mokky.dev/items', {
+      params
+    })
+
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -13,14 +52,15 @@ Header
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (Дешёвые)</option>
-            <option>По цене (Дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (Дешёвые)</option>
+            <option value="-price">По цене (Дорогие)</option>
           </select>
           <div class="relative">
             <img class="absolute left-3 top-3" src="/search.svg" alt="search" />
             <input
+              v-on:input="onChangeSearchInput"
               class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Поиск..."
@@ -28,7 +68,9 @@ Header
           </div>
         </div>
       </div>
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,19 +1,24 @@
 <script setup>
 import Card from './Card.vue'
+
+defineProps({
+  items: Array
+})
+
+const onClickAdd = () => {
+  console.log('some text')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
-    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :image-url="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
+      :onClickAdd="onClickAdd"
+    />
   </div>
 </template>
