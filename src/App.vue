297d6f7957758286235c4f595a4a6a51a7207c62
Artem <artkrasov@gmail.com>
2024-06-30 14:28:59 +0300

    Добавил карточку товара

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
 
 Header
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <CardList />
+    </div>
   </div>
 </template>
 
---
+++ src/components/CardList.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card image-url="/sneakers/sneakers-1.jpg" title="Мужские кросовки" :price="5000" />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
